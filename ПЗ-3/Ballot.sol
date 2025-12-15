4d6ef7200098e8e99bf78f52c00889ad962024a6
nffie <ff3sta@gmail.com>
2025-12-15 06:34:01 +0300

    Refactor ConfidenceVoting contract to BallotCPP

--- ПЗ-3/Ballot.sol
+++ ПЗ-3/Ballot.sol
@@ -1,141 +1,105 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-contract ConfidenceVoting {
+contract BallotCPP {
+
+    struct Voter {
+        bool registered;
+        bool voted;
+        uint[3] weights;
+    }
 
     struct Proposal {
         string name;
-        uint256 totalConfidence;
-        uint256 votersWhoChoseIt;
+        uint totalConfidence;
+        uint supporters;
     }
-
-    struct Voter {
-        bool canVote;
-        bool alreadyVoted;
-        uint256[3] weights;
-    }
-
-    address public chairperson;
-    uint256 public votingEndTime;
 
     Proposal[3] public proposals;
     mapping(address => Voter) public voters;
-    uint256 public totalVoters;
 
-    constructor(string[3] memory proposalNames, uint256 votingDurationSeconds) {
-        require(votingDurationSeconds > 0, "Voting duration must be more than 0 seconds");
+    uint public registeredVoters;
+    uint public votingEnd;
+    address public owner;
 
-        chairperson = msg.sender;
-        votingEndTime = block.timestamp + votingDurationSeconds;
+    constructor(
+        string[3] memory proposalNames,
+        uint votingDurationSeconds
+    ) {
+        owner = msg.sender;
+        votingEnd = block.timestamp + votingDurationSeconds;
 
-        proposals[0].name = proposalNames[0];
-        proposals[1].name = proposalNames[1];
-        proposals[2].name = proposalNames[2];
-    }
-
-    function registerVoter(address voterAddress) external {
-        require(msg.sender == chairperson, "Only chairperson can register voters");
-        require(!voters[voterAddress].canVote, "This person is already registered");
-
-        voters[voterAddress].canVote = true;
-        totalVoters++;
-    }
-
-    function vote(uint256 weight0, uint256 weight1, uint256 weight2) external {
-        require(block.timestamp < votingEndTime, "Voting has ended");
-        require(voters[msg.sender].canVote, "You are not registered to vote");
-        require(!voters[msg.sender].alreadyVoted, "You have already voted");
-
-        uint256 total = weight0 + weight1 + weight2;
-        require(total == 100, "Weights must add up to exactly 100");
-
-        voters[msg.sender].alreadyVoted = true;
-        voters[msg.sender].weights[0] = weight0;
-        voters[msg.sender].weights[1] = weight1;
-        voters[msg.sender].weights[2] = weight2;
-
-        if (weight0 > 0) {
-            proposals[0].totalConfidence += weight0;
-            proposals[0].votersWhoChoseIt++;
-        }
-        if (weight1 > 0) {
-            proposals[1].totalConfidence += weight1;
-            proposals[1].votersWhoChoseIt++;
-        }
-        if (weight2 > 0) {
-            proposals[2].totalConfidence += weight2;
-            proposals[2].votersWhoChoseIt++;
+        for (uint i = 0; i < 3; i++) {
+            proposals[i].name = proposalNames[i];
         }
     }
 
-    function getWinner() external view returns (uint256) {
-        require(block.timestamp >= votingEndTime, "Voting is still open");
+    function registerVoter(address voter) external {
+        require(msg.sender == owner, "Only owner");
+        require(!voters[voter].registered, "Already registered");
 
-        uint256 maxConfidence = 0;
-        uint256 winnerIndex = 999;
-        uint256 proposalsWithMaxConfidence = 0;
+        voters[voter].registered = true;
+        registeredVoters++;
+    }
 
-        for (uint256 i = 0; i < 3; i++) {
+    function vote(uint[3] memory weights) external {
+        require(block.timestamp <= votingEnd, "Voting finished");
+        require(voters[msg.sender].registered, "Not registered");
+        require(!voters[msg.sender].voted, "Already voted");
+
+        uint sum;
+        for (uint i = 0; i < 3; i++) {
+            sum += weights[i];
+        }
+        require(sum == 100, "Sum must be 100");
+
+        voters[msg.sender].voted = true;
+        voters[msg.sender].weights = weights;
+
+        for (uint i = 0; i < 3; i++) {
+            if (weights[i] > 0) {
+                proposals[i].totalConfidence += weights[i];
+                proposals[i].supporters++;
+            }
+        }
+    }
+
+    function winningProposal() external view returns (uint) {
+        require(block.timestamp > votingEnd, "Voting not ended");
+
+        uint maxConfidence = 0;
+        uint winner = 999;
+        bool conflict = false;
+
+        for (uint i = 0; i < 3; i++) {
+            if (proposals[i].supporters * 2 <= registeredVoters) {
+                continue;
+            }
+
             if (proposals[i].totalConfidence > maxConfidence) {
                 maxConfidence = proposals[i].totalConfidence;
-                winnerIndex = i;
-                proposalsWithMaxConfidence = 1;
-            } else if (proposals[i].totalConfidence == maxConfidence && maxConfidence > 0) {
-                proposalsWithMaxConfidence++;
+                winner = i;
+                conflict = false;
+            } else if (
+                proposals[i].totalConfidence == maxConfidence &&
+                maxConfidence != 0
+            ) {
+                conflict = true;
             }
         }
 
-        if (proposalsWithMaxConfidence > 1) {
+        if (winner == 999 || conflict) {
             return 999;
         }
 
-        if (maxConfidence == 0) {
-            return 999;
-        }
-
-        uint256 halfOfVoters = totalVoters / 2;
-        if (proposals[winnerIndex].votersWhoChoseIt > halfOfVoters) {
-            return winnerIndex;
-        }
-
-        return 999;
+        return winner;
     }
 
-    function getVoterWeights(address voterAddress) external view returns (uint256[3] memory) {
-        require(voters[voterAddress].canVote, "This person is not registered");
-        return voters[voterAddress].weights;
-    }
-
-    function getProposalInfo(uint256 proposalId)
+    function voterDetails(address voter)
         external
         view
-        returns (string memory name, uint256 totalConfidence, uint256 votersWhoChoseIt)
+        returns (uint[3] memory)
     {
-        require(proposalId < 3, "Proposal must be 0, 1, or 2");
-        return (
-            proposals[proposalId].name,
-            proposals[proposalId].totalConfidence,
-            proposals[proposalId].votersWhoChoseIt
-        );
+        return voters[voter].weights;
     }
-
-    function isVotingOpen() external view returns (bool) {
-        return block.timestamp < votingEndTime;
-    }
-
-    function secondsUntilVotingEnds() external view returns (uint256) {
-        if (block.timestamp >= votingEndTime) {
-            return 0;
-        }
-        return votingEndTime - block.timestamp;
-    }
-
-    function getTotalVoters() external view returns (uint256) {
-        return totalVoters;
-    }
-
-    function getVotesNeededForMajority() external view returns (uint256) {
-        return (totalVoters / 2) + 1;
-    }
-
 }
